b89a65846150ec0ed4e407caad3f12d2d1821b6f
NIckLee-hanover <42844776+NIckLee-hanover@users.noreply.github.com>
2018-09-26 13:37:54 -0400

    Updated from Brython Server: 9/26/2018 1:37:53 PM

--- stringjumble.py
+++ stringjumble.py
@@ -1,9 +1,9 @@
 """
 stringjumble.py
-Author: <your name>
-Credit: <sources>
+Author: Nick Lee
+Credit: Probably a lot but we'll see
 
-Assignment:
+Assignment: String jumble
 
 The purpose of this challenge is to gain proficiency with 
 manipulating lists.
@@ -25,3 +25,33 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
+
+string = input("Please enter a string of text (the bigger the better): ")
+
+
+def reverse(s): # string reversing function
+    s2 = ""
+    letter = int(len(s)-1)
+    for i in range(len(s)):
+        s2 = ("{0}{1}".format(s2, s[letter]))
+        letter -= 1
+    print(s2)
+
+def word(s,n):
+    s2 = ""
+    while s[n] != " " or n < 0:
+        s2 = ("{0}{1}".format(s2, s[n]))
+        n -= 1
+        print(s[n])
+    del(s2,0)
+
+print(word(string, int(len(string)-1)))
+
+reverse(string)
+
+
+
+
+
+
+
