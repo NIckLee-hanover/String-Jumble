820bf05084c71542ba12e09c6a289f77aa53ea55
NIckLee-hanover <42844776+NIckLee-hanover@users.noreply.github.com>
2018-09-27 12:24:14 -0400

    Updated from Brython Server: 9/27/2018 12:24:12 PM

--- testspace.py
+++ testspace.py
@@ -1,1 +1,47 @@
+"""
+testspace.py
+Author: Nick Lee
+Credit: Probably a lot but we'll see
+
+Assignment: String jumble
+
+The purpose of this challenge is to gain proficiency with 
+manipulating lists.
+
+Write and submit a Python program that accepts a string from 
+the user and prints it back in three different ways:
+
+* With all letters in reverse.
+* With words in reverse order, but letters within each word in 
+  the correct order.
+* With all words in correct order, but letters reversed within 
+  the words.
+
+Output of your program should look like this:
+
+Please enter a string of text (the bigger the better): There are a few techniques or tricks that you may find handy
+You entered "There are a few techniques or tricks that you may find handy". Now jumble it:
+ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
+handy find may you that tricks or techniques few a are There
+erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
+"""
+#string = input("Please enter a string of text (the bigger the better): ")
+s2 = ""
+def word(s,n):
+    while s[n] != " ":
+        s2 = ("{0}{1}".format(s2, s[n]))
+        n += 1
+
+
+word('test this program',0)
+
+
+
+
+
+
+
+
+
+
 
